9b7ff3dd2dbe99cc96215349b9f2c8f64a658858
Alexis1623 <56893401+Alexis1623@users.noreply.github.com>
2023-10-03 17:10:01 -0600

    Lenguajes_Practica 2 Contiene 1 ejemplo en javascript CSS, cualquier duda sobre el mismo favor avisar

---
+++ Practica2_Lenguajes/src/test/java/com/Lenguajes/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lenguajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
