39bf4efa5f55fd9addd2997a1e8067742bfc72a0
jvkevin2001 <123786690+jvkevin2001@users.noreply.github.com>
2023-11-21 17:19:06 -0600

    Kevin-SE IMPLEMENTO EN FirebaseStorageService los comandos y el nombre de la tienda zapateria Patito

--- ProyectoV2/src/main/java/com/proyectoV2/service/FirebaseStorageService.java
+++ ProyectoV2/src/main/java/com/proyectoV2/service/FirebaseStorageService.java
@@ -12,16 +12,16 @@ public interface FirebaseStorageService {
     public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
 
     //El BuketName es el <id_del_proyecto> + ".appspot.com"
-    final String BucketName = "techshop-k-2b436.appspot.com";
+    final String BucketName = "zapateria-patito.appspot.com";
 
     //Esta es la ruta básica de este proyecto Techshop
-    final String rutaSuperiorStorage = "techshop";
+    final String rutaSuperiorStorage = "zapateria-patito";
 
     //Ubicación donde se encuentra el archivo de configuración Json
     final String rutaJsonFile = "firebase";
     
     //El nombre del archivo Json
-    final String archivoJsonFile = "techshop-k-2b436-firebase-adminsdk-dvu2r-1dd883a4d1.json";
+    final String archivoJsonFile = "zapateria-patito-firebase-adminsdk-fae6t-2a91eeafe8.json";
 }
 
 
