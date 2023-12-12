c1c809918df1337d0f61255c60404eaebfe69c40
Kevin Pereira <Kevin Pereira@KevinPereira>
2023-12-12 05:27:04 -0600

    Cambios en el FireBase Storage Service Bucket name, RutaSuperiorStorage, ruta Json File y archioJsonFile

--- Zapateria_CR/src/main/java/com/tienda/Service/FirebaseStorageService.java
+++ Zapateria_CR/src/main/java/com/tienda/Service/FirebaseStorageService.java
@@ -12,16 +12,16 @@ public interface FirebaseStorageService {
     public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
 
     //El BuketName es el <id_del_proyecto> + ".appspot.com"
-    final String BucketName = "techshop-k-2b436.appspot.com";
+    final String BucketName = "zapateriapatito-5e89c.appspot.com";
 
     //Esta es la ruta básica de este proyecto Techshop
-    final String rutaSuperiorStorage = "techshop";
+    final String rutaSuperiorStorage = "Zapateria";
 
     //Ubicación donde se encuentra el archivo de configuración Json
     final String rutaJsonFile = "firebase";
     
     //El nombre del archivo Json
-    final String archivoJsonFile = "techshop-k-2b436-firebase-adminsdk-dvu2r-1dd883a4d1.json";
+    final String archivoJsonFile = "zapateriapatito-5e89c-firebase-adminsdk-lgjep-1fa6fd1347.json";
 }
 
 
